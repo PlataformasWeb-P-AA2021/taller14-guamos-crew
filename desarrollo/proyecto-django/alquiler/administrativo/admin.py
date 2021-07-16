320e1ec9c9707784e430cf64da1d9f2979965116
BrandonVS <vegasotobrandon@yahoo.com>
2021-07-16 10:31:59 -0500

    Admin.py

--- desarrollo/proyecto-django/alquiler/administrativo/admin.py
+++ desarrollo/proyecto-django/alquiler/administrativo/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from administrativo.models import Edificio, Departamento
+
+
+class EdificioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'direccion', 'ciudad', 'tipo')
+    search_fields = ('nombre', 'direccion')
+
+
+admin.site.register(Edificio, EdificioAdmin)
+
+
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'costo_dept', 'num_dept', 'num_cuarto', 'edificio')
+    raw_id_fields = ('edificio',)
+
+
+admin.site.register(Departamento, DepartamentoAdmin)
