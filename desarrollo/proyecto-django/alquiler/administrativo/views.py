e7e17946dfe77c24720f218253eec2a82e63d795
BrandonVS <vegasotobrandon@yahoo.com>
2021-07-16 10:36:47 -0500

    views.py

--- desarrollo/proyecto-django/alquiler/administrativo/views.py
+++ desarrollo/proyecto-django/alquiler/administrativo/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from administrativo.serializers import UserSerializer, GroupSerializer, \
+    EdificioSerializer, DepartamentoSerializer
+
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class EdificioViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+    queryset = Edificio.objects.all()
+    serializer_class = EdificioSerializer
+
+
+class DepartamentoViewSet(viewsets.ModelViewSet):
+    # class DepartamentoViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Departamento.objects.all()
+    serializer_class = DepartamentoSerializer
